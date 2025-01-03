b3dc61d4c411908c9c5621ddec5c8c2176c4b1a8
mahdiyeh <mahi188480@gmail.com>
2025-01-03 14:37:44 +0330

    p4

--- project.cpp
+++ project.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 void ShowMenu(){
 cout<<"1. Start Game"<<endl;
@@ -7,17 +8,32 @@ cout<<"3. Difficulty"<<endl;
 cout<<"4. Help"<<endl;
 cout<<"5. Exit"<<endl;
 }
-void drawMap(){
+void drawMap(int playerX, int playerY){
 for (int i=0 ;i<20 ;i++){
     for(int j=0;j<20;j++){
         if(i==0 || i==19 ||j==0 || j==19){
-            cout<<"X";}
-        else if (i==1 && j==1){
-            cout<<"S";}
-        else{cout<<" ";}
+            cout<<"* ";}
+        else if (i == playerX && j == playerY){
+            cout<<"S ";}
+        else{cout<<"  ";}
     }
     cout<<endl;
 }
+}
+void movePlayer() {
+    int playerX = 1, playerY = 1;
+    char input;
+
+    while (true) {
+        system("cls");
+        drawMap(playerX,playerY);
+        input = _getch();
+        if (input == 'w' && playerX > 1) playerX--;
+        else if (input == 's' && playerX < 18) playerX++;
+        else if (input == 'a' && playerY > 1) playerY--;
+        else if (input == 'd' && playerY < 18) playerY++;
+        else if (input == 'q') break;
+    }
 }
 int main(){
 int choice;
@@ -27,9 +43,7 @@ while(true){
  cout<<"Enter your choice: ";
  cin>>choice;
  if(choice==1){
-    system("cls");
-    drawMap();
-    system("pause");
+    movePlayer();
  }
  else if(choice==5){
     cout<<"Exiting.."<<endl;
