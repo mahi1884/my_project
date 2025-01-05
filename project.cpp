f38feacaf21eb6f1074ba10a3484d6116799449f
mahdiyeh <mahi188480@gmail.com>
2025-01-05 20:39:41 +0330

    p5

--- project.cpp
+++ project.cpp
@@ -9,10 +9,14 @@ cout<<"4. Help"<<endl;
 cout<<"5. Exit"<<endl;
 }
 void drawMap(int playerX, int playerY){
-for (int i=0 ;i<20 ;i++){
-    for(int j=0;j<20;j++){
-        if(i==0 || i==19 ||j==0 || j==19){
+for (int i=0 ;i<21 ;i++){
+    for(int j=0;j<21;j++){
+        if(i==0 || i==20 ||j==0 || j==20){
             cout<<"* ";}
+            else if(i%2==0&&j%2==0){
+            cout<<"XX";}
+            else if (i==10 &&j==10){
+            cout<<"E";}
         else if (i == playerX && j == playerY){
             cout<<"S ";}
         else{cout<<"  ";}
@@ -28,10 +32,10 @@ void movePlayer() {
         system("cls");
         drawMap(playerX,playerY);
         input = _getch();
-        if (input == 'w' && playerX > 1) playerX--;
-        else if (input == 's' && playerX < 18) playerX++;
-        else if (input == 'a' && playerY > 1) playerY--;
-        else if (input == 'd' && playerY < 18) playerY++;
+        if (input == 'w' && playerX > 1&& !(playerX-1==5&&playerY==5)) playerX--;
+        else if (input == 's' && playerX < 18&& !(playerX+1==5&&playerY==5)) playerX++;
+        else if (input == 'a' && playerY > 1&& !(playerX==5&&playerY-1==5)) playerY--;
+        else if (input == 'd' && playerY < 18&&!(playerX==5&&playerY+1==5)) playerY++;
         else if (input == 'q') break;
     }
 }
