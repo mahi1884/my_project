5e6ccf0f3f07c07c209088c5984d81427e54d304
mahdiyeh <mahi188480@gmail.com>
2025-01-06 20:12:59 +0330

    p8

--- project.cpp
+++ project.cpp
@@ -2,10 +2,25 @@
 #include<conio.h>
 #include<windows.h>
 using namespace std;
-char map[21[21];
+char map[21][21];
 int enemyX[3]={5,11,15};
 int enemyY[3]={5,11,15};
 int enemyDir[3]={1,-1,1};
+
+int brickBlocks[][2] = {
+    {1, 2}, {1, 8}, {1, 19}, {1, 18}, {1, 17}, {2, 3}, {2, 7}, {2, 11},
+    {3, 1}, {3, 5}, {3, 8}, {3, 18}, {4, 11}, {4, 19}, {5, 2}, {5, 3},
+    {5, 6}, {5, 9}, {5, 11}, {5, 12}, {5, 13}, {5, 16}, {5, 18}, {6, 5},
+    {6, 1}, {7, 1}, {7, 13}, {8, 1}, {8, 5}, {8, 13}, {8, 19}, {9, 1},
+    {9, 9}, {9, 16}, {10, 1}, {10, 5}, {10, 7}, {10, 9}, {10, 19},
+    {11, 6}, {11, 7}, {11, 12}, {11, 13}, {11, 15}, {11, 16}, {11, 19},
+    {12, 3}, {12, 7}, {12, 13}, {12, 17}, {13, 9}, {13, 10}, {13, 14},
+    {13, 15}, {13, 18}, {14, 1}, {14, 5}, {14, 11}, {15, 3}, {15, 7},
+    {15, 11}, {15, 12}, {15, 13}, {15, 19}, {16, 3}, {16, 11}, {16, 17},
+    {17, 5}, {17, 6}, {17, 14}, {17, 15}, {17, 18}, {18, 1}, {18, 13},
+    {18, 17}, {18, 19}, {19, 1}, {19, 2}, {19, 6}, {19, 7}, {19, 12},
+    {19, 13}, {19, 17}, {19, 18}
+};
 void ShowMenu(){
 cout<<"1. Start Game"<<endl;
 cout<<"2. load Game"<<endl;
@@ -15,12 +30,18 @@ cout<<"5. Exit"<<endl;
 }
 void initializeMap(){
 for(int i=0 ; i<21; i++){
-    for(int j=0 ; j<21;i++){
+    for(int j=0 ; j<21;j++){
         if(i==0 ||i==20||j==0||j==20){map[i][j]='*';}
         else if(i%2==0 && j%2==0){map[i][j]='X';}
-        else if((i==1&&j==3)||(i==1 &&j==8)||(i==1&&j==15)||(i==2 && j==4)||(i==2&&j==9)){map[i][j]='_-';}
-        else{map[i][j]='';}
-    }}}
+        else{map[i][j]=' ';}
+    }}
+    int numBricks = sizeof(brickBlocks) / sizeof(brickBlocks[0]);
+    for (int k = 0; k < numBricks; k++) {
+        int x = brickBlocks[k][0];
+        int y = brickBlocks[k][1];
+        map[x][y] = 'N';  // بلوک آجری
+    }
+}
 void drawMap(int playerX, int playerY){
 for (int i=0 ;i<21 ;i++){
     for(int j=0;j<21;j++){
@@ -30,21 +51,17 @@ for (int i=0 ;i<21 ;i++){
                     (i==enemyX[2] && j==enemyY[2]) ))
                     {cout<<"E ";}
             else if(map[i][j]=='X'){cout<<"XX";}
-        else if (map[i][j]=='_'){cout<<"_- ";}
-        else if (map[i][j]=='*'){cout<<"* "}
+        else if (map[i][j]=='N'){cout<<"N ";}
+        else if (map[i][j]=='*'){cout<<"* ";}
         else{cout<<"  ";}
     }
     cout<<endl;
 }
 }
 bool isWalkable(int x,int y){
-return(map[x][y]==' ');}
-void updateEnemies(){
-for(int i=0; i<3 ; i++)
-    if(enemyDir[i]==1&& enemyY[i]<19){enemyY[i]++;}
-     else if (enemyDir[i] == -1 && enemyY[i] > 1) {enemyY[i]--;}
-         else {enemyDir[i] *= -1;}
-    }
+return(map[x][y]==' ');
+}
+
     void updateEnemies(){
     for(int i=0 ;i<3 ;i++){
           if (enemyDir[i] == 1 && isWalkable(enemyX[i], enemyY[i] + 1)) {enemyY[i]++;}
@@ -72,11 +89,11 @@ void movePlayer() {
 }
 }
 int main(){
-    initializeMap()
+initializeMap();
 int choice;
 while(true){
  system("cls");
- ShowMenu();
+ShowMenu();
  cout<<"Enter your choice: ";
  cin>>choice;
  if(choice==1){
